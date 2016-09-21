e166b661e0a4b9ea58ed1f529cb46a7543b5add0
Nguyen Van Thieu B <nguyen.van.thieub@framgias-Mac-mini-6.local>
2016-09-21 17:13:34 +0700

    [02] Menu

--- Pronunciation/Controller/MenuViewController.m
+++ Pronunciation/Controller/MenuViewController.m
@@ -16,22 +16,27 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view.
 }
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+#pragma mark - Click Button
+
+- (IBAction)btChart:(id)sender {
+    UIStoryboard *sb = [UIStoryboard storyboardWithName:@"Chart" bundle:nil];
+    UIViewController *vc = [sb instantiateViewControllerWithIdentifier:@"ChartViewController"];
+    [self presentViewController:vc animated:YES completion:nil];
 }
 
-/*
-#pragma mark - Navigation
-
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
+- (IBAction)btPractice:(id)sender {
+    NSLog(@"Practice");
 }
-*/
+
+- (IBAction)btQuiz:(id)sender {
+    NSLog(@"Quiz");
+}
+
+- (IBAction)btMore:(id)sender {
+    NSLog(@"More");
+}
+
 
 @end
